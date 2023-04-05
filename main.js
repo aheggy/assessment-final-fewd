1d4b2c6a3a2860e09af4e68af6b7d0d75f2d991c
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-05 11:06:50 -0400

    Add comments to JS file

--- main.js
+++ main.js
@@ -1,35 +1,28 @@
 
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+const BASE_URL = `https://resource-ghibli-api.onrender.com`
 const movieSelector = document.getElementById("titles") 
 const detailContainer = document.getElementById("display-info")
-
 const commentInput = document.getElementById("review")
-
 const form = document.querySelector("form")
-
 const commentList = document.getElementById("reviews")
-
 const restReviews = document.getElementById("reset-reviews")
-
 const showPeopleButton = document.getElementById("show-people")
+let peopleList = document.getElementById("names")
+
 
 function run() {
 
     
  // Add code you want to run on page load here
- fetch(`https://resource-ghibli-api.onrender.com/films`)
+ fetch(`${BASE_URL}/films`)
 .then(response => response.json())
 .then(response => {
     populateFormDropdown(response)
 })
 .catch(err => console.error(err));
 
-
+// get the movies list
 let populateFormDropdown = movies => {
     for (const movie of movies) {
         // if (counter <= 13) {
@@ -41,13 +34,13 @@ let populateFormDropdown = movies => {
     }
     
 }
-//detect any change in the drop menu and update the description 
+//detect any change in the droplist and update the description 
 let selectedMovie = null
 movieSelector.addEventListener("change", (event) => {
     let movieId = event.target.value
     if (movieId !== "") {
-        // get the details of selected movie using the API
-        fetch(`https://resource-ghibli-api.onrender.com/films/${movieId}`)
+        // fetch API for the details of selected movie
+        fetch(`${BASE_URL}/films/${movieId}`)
         .then(response => response.json())
         .then(response => {
             selectedMovie = response
@@ -61,6 +54,7 @@ movieSelector.addEventListener("change", (event) => {
     }
 })
 
+//create the element and append it to the movie details section
 function updateMovieSelected(movie) {
     detailContainer.textContent = ""
     const title = movie.title
@@ -78,8 +72,14 @@ function updateMovieSelected(movie) {
     detailContainer.append(titleHeader)
     detailContainer.append(releaseYearparagraph)
     detailContainer.append(movieDescription)
+
+    // clear the previous people list if the selected movie change from droplist
+    if (peopleList.innerHTML !== "")  {
+        peopleList.innerHTML = ""
+    } 
 }
 
+// create the comment element and append it to the reviews section
 form.addEventListener("submit", event => {
     event.preventDefault()
    
@@ -90,56 +90,36 @@ form.addEventListener("submit", event => {
         commentList.append(newComment)
         form.reset()
 
+        // clear the reviews section if the rest button clicked
         restReviews.addEventListener("click", event => {
             newComment.remove()
         })
-
-
-
-        
-
-        showPeopleButton.addEventListener("click", event => {
-            const peopleList = document.getElementById("names")
-            peopleList.innerHTML = ""
-        
-            console.log(selectedMovie)
-        
-            selectedMovie.people.forEach( element => {
-                let list = document.createElement("li")
-                fetch(`https://resource-ghibli-api.onrender.com${element}`)
-                .then(response => response.json())
-                .then(response => {
-                    if (response.name){
-                        list.innerText = response.name
-                    } else {
-                        list.innerText = "No People"   
-                    }
-                    peopleList.append(list)
-            })
-            .catch(err => console.error(err));
-        
-            });
-           
-        })
-
-
-
-
-
-
-
-
-        
+        // alert the user that movie should be selected before write a review
     } else {
         alert("Please select a movie first.")
     }
-    
+     
 })
 
+// create list element and fetch the data to show  people for movie
+showPeopleButton.addEventListener("click", event => {
+    peopleList.innerHTML = ""
+    console.log(selectedMovie)
 
+    selectedMovie.people.forEach( element => {
+        let list = document.createElement("li")
+        console.log(element)
+        fetch(`${BASE_URL}${element}`)
+        .then(response => response.json())
+        .then(response => {
+            list.innerText = response.name
+            peopleList.append(list)
+    })
+    .catch(err => console.error(err));
 
-
-selectedMovie = ""
+    });
+   
+})
 
 }
 
